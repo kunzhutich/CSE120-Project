b182e35dfc5fedb5ea6b55b1cd00795ce5a14862
Christian Duan <christianduan2002@gmail.com>
2024-03-11 22:07:56 -0700

    Worked on NavBar and LogIn

--- nextjs-blog/components/loginComponent.js
+++ nextjs-blog/components/loginComponent.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Container, TextField, Button, Box} from '@mui/material';
+import {TextField, Button, Box} from '@mui/material';
+import Link from 'next/link';
 
 export default function LoginPage() {
   const handleSubmit = (event) => {
@@ -8,33 +9,60 @@ export default function LoginPage() {
   };
 
   return (
-    <Container maxWidth="sm">
-      <form onSubmit={handleSubmit}>
-        <TextField
-          label="Username"
-          variant="outlined"
-          margin="normal"
-          required
-          fullWidth
-        />
-        <TextField
-          label="Password"
-          variant="outlined"
-          type="password"
-          margin="normal"
-          required
-          fullWidth
-        />
-        <Box display="flex" justifyContent="center">
-        <Button
-          type="submit"
-          variant="contained"
-          color="primary"
-        >
-          Sign in
-        </Button>
+    <Box 
+    sx={{
+        backgroundColor: '#00587c',
+        minHeight: '100vh',
+        justifyContent: 'center',
+        flexDirection: 'column',
+        display: 'flex',
+        alignItems: 'center'
+      }}>
+        <Box>
+            <img src="/logo.png"
+            alt="Logo"
+            style={{width:'550px', height:'auto', marginBottom:'50px'}}/>
         </Box>
-      </form>
-    </Container>
+        <Box 
+        maxWidth="medium"
+        sx={{
+            backgroundColor:'#d9d9d9', 
+            borderRadius:'10px', 
+            padding:3,}}>
+        <form onSubmit={handleSubmit}>
+            <TextField
+            label="Username"
+            variant="outlined"
+            margin="normal"
+            // required
+            fullWidth
+            />
+            <TextField
+            label="Password"
+            variant="outlined"
+            type="password"
+            margin="normal"
+            // required
+            fullWidth
+            />
+            <Box display="flex" justifyContent="center">
+                <Link href="/fsorders">
+                    <Button
+                        type="submit"
+                        variant="contained"
+                        color="primary"
+                        sx={{marginBottom:'10px'}}>
+                    Sign in
+                    </Button>
+                </Link>
+            </Box>
+            <Box display="flex" justifyContent="center" mb>
+                <Link href="/forgot-password">
+                    Forgot Password?
+                </Link>
+            </Box>
+        </form>
+        </Box>
+    </Box>
   );
 }
---
+++ nextjs-blog/pages/fsorders.js
@@ -0,0 +1,8 @@
+import NavBar from "../components/navBar";
+export default function fsorders() {
+    return (
+        <div>
+            <NavBar/>
+        </div>
+    );
+}
