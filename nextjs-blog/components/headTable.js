bd70d051c7a8c8d6e0390a67ce639b816e566401
kunzhutich <131626756+kunzhutich@users.noreply.github.com>
2024-04-24 19:36:38 -0700

    Changed columns' field names to match backend

--- nextjs-blog/components/headTable.js
+++ nextjs-blog/components/headTable.js
@@ -6,58 +6,26 @@ import CustomToolbar from './CustomToolbar'; // Import the CustomToolbar compone
 // Creates column definitions for the DataGrid
 
 const columns = [
-  { field: 'id', headerName: '', width: 30, hide: true, headerClassName: 'super-app-theme--header' },
-  { field: 'head', headerName: 'Head', flex: 2, headerClassName: 'super-app-theme--header' },
-  { field: 'lateral', headerName: 'Lateral', flex: 1, headerClassName: 'super-app-theme--header' },
-  { field: 'name', headerName: 'Contact', flex: 2, headerClassName: 'super-app-theme--header' },
-  { field: 'phone', headerName: 'Phone', flex: 1, headerClassName: 'super-app-theme--header' },
-  { field: 'rqstFlo', headerName: 'Rqst Flo', flex: 1, headerClassName: 'super-app-theme--header' },
-  { field: 'hours', headerName: 'Hours', flex: 1, headerClassName: 'super-app-theme--header' },
-  { field: 'estStart', headerName: 'Est Start', editable: true, flex: 1, headerClassName: 'super-app-theme--header' },
-  { field: 'primeDate', headerName: 'Prime Date', editable: true, flex: 1, headerClassName: 'super-app-theme--header' },
-  { field: 'primeTime', headerName: 'Prime Time', editable: true, flex: 1, headerClassName: 'super-app-theme--header' },
-  { field: 'startDate', headerName: 'Start Date', editable: true, flex: 1, headerClassName: 'super-app-theme--header' },
-  { field: 'startTime', headerName: 'Start Time', editable: true, flex: 1, headerClassName: 'super-app-theme--header' },
-  { field: 'finishDate', headerName: 'Finish Date', editable: true, flex: 1, headerClassName: 'super-app-theme--header' },
-  { field: 'finishTime', headerName: 'Finish Time', editable: true,  flex: 1, headerClassName: 'super-app-theme--header' },
-  { field: 'primeTotal', headerName: 'Prime Total', flex: 1, headerClassName: 'super-app-theme--header' },
-  { field: 'totalHour', headerName: 'Total Hour', flex: 1, headerClassName: 'super-app-theme--header' },
-  { field: 'called', headerName: 'Called', editable: true, flex: 1, headerClassName: 'super-app-theme--header' },
+    { field: 'id', headerName: 'Combo', width: 130, hide: true, headerClassName: 'super-app-theme--header' },
+    { field: 'lat', headerName: 'Lateral', flex: 1, headerClassName: 'super-app-theme--header' },
+    { field: 'sg', headerName: 'SG', flex: 1, headerClassName: 'super-app-theme--header' },
+    { field: 'name', headerName: 'Contact', flex: 2, headerClassName: 'super-app-theme--header' },
+    { field: 'phone', headerName: 'Phone', flex: 1, headerClassName: 'super-app-theme--header' },
+    { field: 'flow', headerName: 'Rqst Flo', flex: 1, headerClassName: 'super-app-theme--header' },
+    { field: 'hours', headerName: 'Hours', flex: 1, headerClassName: 'super-app-theme--header' },
+    { field: 'est_start', headerName: 'Est Start', editable: true, flex: 1, headerClassName: 'super-app-theme--header' },
+    { field: 'prime_date', headerName: 'Prime Date', editable: true, flex: 1, headerClassName: 'super-app-theme--header' },
+    { field: 'prime_time', headerName: 'Prime Time', editable: true, flex: 1, headerClassName: 'super-app-theme--header' },
+    { field: 'start_date', headerName: 'Start Date', editable: true, flex: 1, headerClassName: 'super-app-theme--header' },
+    { field: 'start_time', headerName: 'Start Time', editable: true, flex: 1, headerClassName: 'super-app-theme--header' },
+    { field: 'finish_date', headerName: 'Finish Date', editable: true, flex: 1, headerClassName: 'super-app-theme--header' },
+    { field: 'finish_time', headerName: 'Finish Time', editable: true,  flex: 1, headerClassName: 'super-app-theme--header' },
+    { field: 'prime_total', headerName: 'Prime Total', flex: 1, headerClassName: 'super-app-theme--header' },
+    { field: 'total_hours', headerName: 'Total Hour', flex: 1, headerClassName: 'super-app-theme--header' },
+    { field: 'called', headerName: 'Called', editable: true, flex: 1, headerClassName: 'super-app-theme--header' },
 ];
 
-// Creates row data for the DataGrid
-//const rows = [
-//  { id: 1, head: '001002003 -654258', lateral: 'CM', sg: '06-05',
-//    contact: 'George Washington', phoneNumber: '111-2345', rqstFlo: '16.77',
-//    hours: '11', estStart: 'Thu 1430', primeDate: '0208', primeTime: '0635',
-//    startDate: '0208', startTime: '1430', finishDate: '0208', finishTime: '1735',
-//    primeTotal: '7:55', totalHour: '3:05', called: 'o'
-//  },
-//  { id: 2, head: '001002003 -654258', lateral: 'CM', sg: '06-05',
-//    contact: 'George Washington', phoneNumber: '111-2345', rqstFlo: '16.77',
-//    hours: '11', estStart: 'Thu 1430', primeDate: '0208', primeTime: '0635',
-//    startDate: '0208', startTime: '1430', finishDate: '0208', finishTime: '1735',
-//    primeTotal: '7:55', totalHour: '3:05', called: 'o'
-//  },
-//  { id: 3, head: '001002003 -654258', lateral: 'CM', sg: '06-05',
-//    contact: 'George Washington', phoneNumber: '111-2345', rqstFlo: '16.77',
-//    hours: '11', estStart: 'Thu 1430', primeDate: '0208', primeTime: '0635',
-//    startDate: '0208', startTime: '1430', finishDate: '0208', finishTime: '1735',
-//    primeTotal: '7:55', totalHour: '3:05', called: 'o'
-//  },
-//  { id: 4, head: '001002003 -654258', lateral: 'CM', sg: '06-05',
-//    contact: 'George Washington', phoneNumber: '111-2345', rqstFlo: '16.77',
-//    hours: '11', estStart: 'Thu 1430', primeDate: '0208', primeTime: '0635',
-//    startDate: '0208', startTime: '1430', finishDate: '0208', finishTime: '1735',
-//    primeTotal: '7:55', totalHour: '3:05', called: 'o'
-//  },
-//  { id: 5, head: ' ', lateral: ' ', sg: ' ',
-//    contact: ' ', phoneNumber: ' ', rqstFlo: ' ',
-//    hours: ' ', estStart: ' ', primeDate: ' ', primeTime: ' ',
-//    startDate: ' ', startTime: ' ', finishDate: ' ', finishTime: ' ',
-//    primeTotal: ' ', totalHour: '', called: ' '
-//  }
-//];
+
 
 export default function HeadTable(props) {
     const { requiredString, headerColor  } = props;
