464f8248ff96971ea0a2604468410b3f7fc84c95
Noorulain Irshad <nirshad@ucmerced.edu>
2024-04-25 18:39:59 -0700

    yayyy dropdown is styled 👾

--- nextjs-blog/components/fTable.js
+++ nextjs-blog/components/fTable.js
@@ -13,7 +13,9 @@ import DialogContentText from '@mui/material/DialogContentText';
 import DialogActions from '@mui/material/DialogActions';
 import Button from '@mui/material/Button';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
+import { makeStyles } from '@mui/styles';
 
+  
 const DatePickerCell = ({ value, id, onCellValueChange }) => {
     const [selectedDate, setSelectedDate] = useState(value ? dayjs(value) : null);
 
@@ -106,7 +108,7 @@ const HeadEditor = ({ value, onCellValueChange, id }) => {
     };
 
     return (
-        <select value={value || ''} onChange={handleChange}>
+        <select style={{ backgroundColor: 'lightblue', color: 'black', padding: '5px', borderRadius: '5px', fontSize: '14px' }} value={value || ''} onChange={handleChange}>
             <option value="">Select...</option>
             {headOptions.map((option) => (
                 <option key={option.value} value={option.value}>
@@ -150,26 +152,26 @@ const handleCellEditCommit = async (updatedRow) => {
 
 
 // Define the columns for the DataGrid
-const columns = [
-    { field: 'id', headerName: 'Combo', width: 130, flex: 2, headerClassName: 'super-app-theme--header' },
-    { field: 'lat', headerName: 'Lat', flex: 1, headerClassName: 'super-app-theme--header' },
-    { field: 'sg', headerName: 'SG', flex: 1, headerClassName: 'super-app-theme--header' },
-    { field: 'name', headerName: 'Name', flex: 2, headerClassName: 'super-app-theme--header' },
-    { field: 'phone', headerName: 'Phone', flex: 1, headerClassName: 'super-app-theme--header' },
-    { field: 'flow', headerName: 'Flow', flex: 1, headerClassName: 'super-app-theme--header' },
-    { field: 'hours', headerName: 'Hours', flex: 1, headerClassName: 'super-app-theme--header' },
-    { field: 'acre', headerName: 'Acre', flex: 1, headerClassName: 'super-app-theme--header' },
-    { field: 'crop', headerName: 'Crop', flex: 1, headerClassName: 'super-app-theme--header' },
-    { field: 'type', headerName: 'Type', flex: 1, headerClassName: 'super-app-theme--header' },
-    { field: 'date', headerName: 'Date', editable: true, flex: 1, headerClassName: 'super-app-theme--header' },
-    { field: 'comment', headerName: 'Comment', editable: true,  flex: 1.5, renderCell: (params) => <CommentsCell {...params} />, headerClassName: 'super-app-theme--header' },
-    { field: 'sbxcfs', headerName: 'SBXCFS', flex: 1, headerClassName: 'super-app-theme--header' },
-    { field: 'head', headerName: 'Head', editable: true, flex: 1.5, 
-    renderCell: (params)=> <HeadEditor value = {params.value} 
-    onCellValueChange= {(newValue) => params.api.setValue(params.id, 'head', newValue)} />, headerClassName: 'super-app-theme--header' },
-    { field: 'estStart', headerName: 'Est Start', flex: 1, headerClassName: 'super-app-theme--header', renderCell: (params) => <DatePickerCell />  },
-    { field: 'estStop', headerName: 'Est Stop', editable: true, flex: 1, headerClassName: 'super-app-theme--header' },
-];
+// const columns = [
+//     { field: 'id', headerName: 'Combo', width: 130, flex: 2, headerClassName: 'super-app-theme--header' },
+//     { field: 'lat', headerName: 'Lat', flex: 1, headerClassName: 'super-app-theme--header' },
+//     { field: 'sg', headerName: 'SG', flex: 1, headerClassName: 'super-app-theme--header' },
+//     { field: 'name', headerName: 'Name', flex: 2, headerClassName: 'super-app-theme--header' },
+//     { field: 'phone', headerName: 'Phone', flex: 1, headerClassName: 'super-app-theme--header' },
+//     { field: 'flow', headerName: 'Flow', flex: 1, headerClassName: 'super-app-theme--header' },
+//     { field: 'hours', headerName: 'Hours', flex: 1, headerClassName: 'super-app-theme--header' },
+//     { field: 'acre', headerName: 'Acre', flex: 1, headerClassName: 'super-app-theme--header' },
+//     { field: 'crop', headerName: 'Crop', flex: 1, headerClassName: 'super-app-theme--header' },
+//     { field: 'type', headerName: 'Type', flex: 1, headerClassName: 'super-app-theme--header' },
+//     { field: 'date', headerName: 'Date', editable: true, flex: 1, headerClassName: 'super-app-theme--header' },
+//     { field: 'comment', headerName: 'Comment', editable: true,  flex: 1.5, renderCell: (params) => <CommentsCell {...params} />, headerClassName: 'super-app-theme--header' },
+//     { field: 'sbxcfs', headerName: 'SBXCFS', flex: 1, headerClassName: 'super-app-theme--header' },
+//     { field: 'head', headerName: 'Head', editable: true, flex: 1.5, 
+//     renderCell: (params)=> <HeadEditor value = {params.value} 
+//     onCellValueChange= {(newValue) => params.api.setValue(params.id, 'head', newValue)} />, headerClassName: 'super-app-theme--header' },
+//     { field: 'estStart', headerName: 'Est Start', flex: 1, headerClassName: 'super-app-theme--header', renderCell: (params) => <DatePickerCell />  },
+//     { field: 'estStop', headerName: 'Est Stop', editable: true, flex: 1, headerClassName: 'super-app-theme--header' },
+// ];
 
 export default function FTable() {
     const [orders, setOrders] = useState([]);
